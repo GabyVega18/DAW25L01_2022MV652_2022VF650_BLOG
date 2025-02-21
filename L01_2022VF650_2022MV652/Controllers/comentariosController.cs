8d937ea94868abeaaeb6b83bc3d3e615d754dda4
Gabriela Menendez <gabyvega2024@hotmail.com>
2025-02-20 21:01:39 -0600

    nuevos cambios en los controladores

--- L01_2022VF650_2022MV652/Controllers/comentariosController.cs
+++ L01_2022VF650_2022MV652/Controllers/comentariosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using L01_2022VF650_2022MV652.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2022VF650_2022MV652.Controllers
 {
@@ -43,6 +44,60 @@ namespace L01_2022VF650_2022MV652.Controllers
 
             return Ok(comentario);
 
+        }
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarComentarios([FromBody] comentarios comentario)
+        {
+            try
+            {
+                _blogDBContexto.comentarios.Add(comentario);
+                _blogDBContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarComentarios(int id, [FromBody] comentarios comentarioModificar)
+        {
+            comentarios? comentarioActual = (from e in _blogDBContexto.comentarios
+                                                  where e.comentariosId == id
+
+                                                  select e).FirstOrDefault();
+            if (comentarioActual == null)
+            { return NotFound(); }
+
+            comentarioActual.publicacionId = comentarioModificar.publicacionId;
+            comentarioActual.comentario = comentarioModificar.comentario;
+            comentarioActual.usuarioId = comentarioModificar.usuarioId;
+
+            _blogDBContexto.Entry(comentarioActual).State = EntityState.Modified;
+            _blogDBContexto.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+
+        public IActionResult EliminarComentario(int id)
+        {
+            comentarios? comentario = (from e in _blogDBContexto.comentarios
+                                             where e.comentariosId == id
+                                             select e).FirstOrDefault();
+            if (comentario == null)
+                return NotFound();
+            _blogDBContexto.comentarios.Attach(comentario);
+            _blogDBContexto.comentarios.Remove(comentario);
+            _blogDBContexto.SaveChanges();
+            return Ok(comentario);
         }
     }
 }
--- L01_2022VF650_2022MV652/Controllers/usuariosController.cs
+++ L01_2022VF650_2022MV652/Controllers/usuariosController.cs
@@ -44,5 +44,60 @@ namespace L01_2022VF650_2022MV652.Controllers
 
             return Ok(usuario);
         }
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarUsuario([FromBody] usuarios usuario)
+        {
+            try
+            {
+                _blogDBContexto.usuarios.Add(usuario);
+                _blogDBContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarUsuario(int id, [FromBody] usuarios usuarioModificar)
+        {
+            usuarios? usuarioActual = (from e in _blogDBContexto.usuarios
+                                 where e.usuarioId == id
+                                 select e).FirstOrDefault();
+
+            if (usuarioActual == null)
+            { return NotFound(); }
+
+            usuarioActual.nombreUsuario = usuarioModificar.nombreUsuario;
+            usuarioActual.clave = usuarioModificar.clave;
+            usuarioActual.nombre = usuarioModificar.nombre;
+            usuarioActual.apellido = usuarioModificar.apellido;
+
+            _blogDBContexto.Entry(usuarioActual).State = EntityState.Modified;
+            _blogDBContexto.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+
+        public IActionResult EliminarUsuario(int id)
+        {
+            usuarios? usuario = (from e in _blogDBContexto.usuarios
+                                       where e.usuarioId == id
+                                       select e).FirstOrDefault();
+            if (usuario == null)
+                return NotFound();
+            _blogDBContexto.usuarios.Attach(usuario);
+            _blogDBContexto.usuarios.Remove(usuario);
+            _blogDBContexto.SaveChanges();
+            return Ok(usuario);
+        }
     }
 }
