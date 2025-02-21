53126a3ddca7101ababf1e4016ccb79ccc77e6db
Gabriela Menendez <gabyvega2024@hotmail.com>
2025-02-20 20:46:05 -0600

    se agraron cambios

--- L01_2022VF650_2022MV652/Controllers/calificacionesController.cs
+++ L01_2022VF650_2022MV652/Controllers/calificacionesController.cs
@@ -1,6 +1,7 @@
 ﻿using L01_2022VF650_2022MV652.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L01_2022VF650_2022MV652.Controllers
 {
@@ -43,5 +44,62 @@ namespace L01_2022VF650_2022MV652.Controllers
 
             return Ok(calificacion);
         }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarCalificaciones([FromBody] calificaciones calificacion)
+        {
+            try
+            {
+                _blogDBContexto.calificaciones.Add(calificacion);
+                _blogDBContexto.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarCalificaciones(int id, [FromBody] calificaciones calificacionModificar) 
+        {
+            calificaciones? calificacionActual = (from e in _blogDBContexto.calificaciones
+                                            where e.calificacionId == id
+             
+                                            select e).FirstOrDefault();
+            if (calificacionActual == null)
+            {  return NotFound(); }
+
+            calificacionActual.publicacionId = calificacionModificar.publicacionId;
+            calificacionActual.usuarioId = calificacionModificar.usuarioId;
+            calificacionActual.calificacion = calificacionModificar.calificacion;
+
+            _blogDBContexto.Entry(calificacionActual).State = EntityState.Modified;
+            _blogDBContexto.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id) 
+        {
+            calificaciones? calificacion = (from e in _blogDBContexto.calificaciones
+                                                  where e.calificacionId == id
+                                                  select e).FirstOrDefault();
+            if(calificacion == null)
+                return NotFound();
+            _blogDBContexto.calificaciones.Attach(calificacion);
+            _blogDBContexto.calificaciones.Remove(calificacion);
+            _blogDBContexto.SaveChanges();
+            return Ok(calificacion);
+        }
+
+
     }
+
 }
